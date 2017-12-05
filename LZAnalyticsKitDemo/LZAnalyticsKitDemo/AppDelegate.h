c814f4aae16f2bf2d7349ce5e061d0b01289ccec
LeoZ <zhouji@yanyukj.cn>
2017-12-05 15:16:12 +0800

    提交工程文件

---
+++ LZAnalyticsKitDemo/LZAnalyticsKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZAnalyticsKitDemo
+//
+//  Created by LeoZ on 2017/12/4.
+//  Copyright © 2017年 LZAnalytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
